772bac8dbcd128e5c73df62d4ca15dd8b1b84ae6
Sanket Saurav <sanketsaurav@gmail.com>
2013-08-14 03:06:44 +0530

    Added dependencies. Completed Facebook login.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,33 @@
 var GenieServices = angular.module('Genie', []);
 
+var g = {connected: false};
+
+function AuthorController($scope) {
+	$scope.author = "Sanket";
+}
+
+function LoginController($scope) {
+
+	/*$scope.genieInit = function() {
+		// Initialize Genie
+		if (g.connected!=true) {
+			genieConnect();
+		}
+	}*/
+
+	$scope.genieConnect = function () {
+		if (g.connected != true) {
+			fbLogin();
+			if (g.status === 'connected') {
+				g.connected = true;
+			}
+		}
+	}
+
+	fbLogin = function () {
+		FB.login(function(response) {
+			g.authResponse = response.authResponse;
+			g.status = response.status;
+		}, {scope: 'read_stream,publish_actions,publish_stream'});
+	}
+}
